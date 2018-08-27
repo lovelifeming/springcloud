94250a899baded5861c36e55d5345ec49400f8fe
zengsm <453752303@qq.com>
2018-08-27 21:42:02 +0800

    Add eureka-server module.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SpringCloud
 The Spring Cloud project built and test. The Spring Cloud project demo and framework learning.
+基于Eureka注册服务提供者和消费者，使用Feign、Ribbon、Hystrix实现服务间的调用、负载均衡及服务熔断和降级功能
+eureka-server  服务注册中心
+eureka-consumer 消费者服务
+eureka-provider 生产者服务
+gateway   统一网关服务
+turbine-server 聚合监控服务
+zipkin-server 基本链路分析服务
+zipkin-stream-server 基于RabbitMQ、Elasticsearch的链路分析服务
---
+++ SimpleDemo/eureka-server/src/test/java/com/zsm/eurekaserver/EurekaServerApplicationTest.java
@@ -0,0 +1,23 @@
+package com.zsm.eurekaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+/**
+ * @Author: zengsm.
+ * @Description:
+ * @Date:Created in 2018/8/11 11:23.
+ * @Modified By:
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServerApplicationTest
+{
+    @Test
+    public void contextLoads()
+    {
+    }
+}
