cff7785fb1f03c19f019fe95276590fdaf695857
zengshengming <453752303@qq.com>
2019-04-18 09:40:47 +0800

    Build the basic framework,add eureka server,eureka client,zuul,ribbon user and other modules.

---
+++ SimpleDemo/eureka-client/src/main/java/com/zsm/eurekaclient/controller/TestEureka.java
@@ -0,0 +1,30 @@
+package com.zsm.eurekaclient.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @Author :zengsm.
+ * @Description :
+ * @Date:Created in 2019/4/15 11:36.
+ * @Modified By :
+ */
+@RestController
+@RequestMapping("/test/")
+public class TestEureka
+{
+    @GetMapping("getUser")
+    public String testGetUser(String name)
+    {
+        return "获取测试通过:" + name;
+    }
+
+    @PostMapping("postUserId")
+    public String testPostUser(String userId)
+    {
+        return "获取用户Id：" + userId;
+    }
+}
